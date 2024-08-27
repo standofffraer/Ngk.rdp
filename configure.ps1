766f35f8646b1be8972b482846efa2077ebb24d5
standofffraer <56636883+standofffraer@users.noreply.github.com>
2024-08-27 22:27:40 +0300

    Update configure.ps1

--- configure.ps1
+++ configure.ps1
@@ -4,7 +4,7 @@
 Write-Output "[INFO] Script started!"
 
 # First we download ngrok
-Invoke-WebRequest -Uri https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -OutFile ngrok.zip
+Invoke-WebRequest -Uri https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip -OutFile ngrok.zip
 
 # Then we unzip it
 Expand-Archive -LiteralPath '.\ngrok.zip'
